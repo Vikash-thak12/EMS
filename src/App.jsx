5571131d6e10dba417674c50f044b8a4443f8d5e
Vikash Thakur <thakvikash622@gmail.com>
2024-10-26 20:30:48 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import LoginPage from "./components/auth/login"
+import EmployeeDash from "./components/Dashboard/EmployeeDash"
 
 function App() {
 
   return (
     <>
-    <LoginPage />
+    {/* <LoginPage /> */}
+    <EmployeeDash />
     </>
   )
 }
